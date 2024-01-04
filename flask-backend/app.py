f5e911d097c872e9af8fa725e77404687132b3a1
Win Tun <wintun.hlaing@outlook.com>
2024-01-04 18:15:05 +0630

    jwt authentication route success

--- flask-backend/app.py
+++ flask-backend/app.py
@@ -4,6 +4,7 @@ from server import create_app
 from server.route.server_test import test_bp
 from server.route.user.register import register_bp
 from server.route.user.auth import auth_bp
+from server.route.authorize_test.authorize import authorize_test_bp
 
 
 #create app instance
@@ -14,6 +15,7 @@ app = create_app()
 app.register_blueprint(test_bp)
 app.register_blueprint(register_bp)
 app.register_blueprint(auth_bp)
+app.register_blueprint(authorize_test_bp)
 
 if __name__ == "__main__":
     #add host as 0.0.0.0 to be accessible from outside when running as a #container
--- flask-backend/server/route/authorize_test/authorize.py
+++ flask-backend/server/route/authorize_test/authorize.py
@@ -6,6 +6,31 @@ from server.server_config import AUTH_API_LINK
 #import token services
 from flask_jwt_extended import (
                                 jwt_required,
-                                get_jwt_identity,
-                                unset_jwt_cookies
+                                get_jwt
                                 )
+
+#register blue print
+authorize_test_bp = Blueprint('authorize_test', __name__,url_prefix=AUTH_API_LINK)
+
+
+@authorize_test_bp.route("/authorize",methods=['POST'])
+@jwt_required()    #Require a valid access token for this route
+def test_authorize_post():
+    req_body = request.get_json()
+    try:
+        result = get_jwt()
+        response ={**req_body,**result}
+        return make_response(jsonify(response))
+    except Exception as e:
+        raise e
+    
+@authorize_test_bp.route("/authorize",methods=['GET'])
+@jwt_required()    #Require a valid access token for this route
+def test_authorize_get():
+    try:
+        result = get_jwt()
+        response ={**result}
+        return make_response(jsonify(response))
+    except Exception as e:
+        raise e
+    
